b15fbce7f10df8bff08932dd7dc998690ab94cce
George Lucchese <george_lucchese@tamu.edu>
2011-04-10 18:18:53 -0500

    Fixing things on document grid

---
+++ src/srl/visgo/util/chat/listeners/StatusChangeListener.java
@@ -0,0 +1,22 @@
+package srl.visgo.util.chat.listeners;
+
+import java.util.EventListener;
+
+import org.jivesoftware.smack.packet.Presence;
+
+/***
+ * Interface for the listener which will notify the status of the users 
+ * working with the visgo
+ * @author manoj
+ *
+ */
+public interface StatusChangeListener extends EventListener{
+
+	/**
+	 * Status change event
+	 * @param userID - email id to which the status belongs to
+	 * @param status - Status is the one of the following - Available, Unavailable and so on 
+	 * 				- it is one of the values of Presence.Type constants.
+	 */
+	public void StatusChanged(String userID, Presence.Type status);
+}
